249d5a79fde3e12443ea43ef1e1f490787c51c02
Brunodiaz10 <167319977+Brunodiaz10@users.noreply.github.com>
2024-11-16 23:52:23 -0300

    Add files via upload

---
+++ tpfinalparte2/tpfinalparte2.js
@@ -0,0 +1,28 @@
+//jazmin Tava
+//Bruno diaz
+//Comision 1
+
+
+//entregamos el juego principal, nos faltaria las imagenes, sonido y la pantallas de inicio
+//se mueve con las flechas del teclado, debe atrapar 10 flores(cubos) amarillos antes del tiempo en pantalla
+
+
+let juego;
+
+function setup() {
+  createCanvas(640, 480); // Crea un lienzo de 640x480 píxeles
+  juego = new Juego(); // Inicializa el juego al crear la instancia de la clase Juego
+}
+
+function draw() {
+  background(220); // Dibuja el fondo (color gris claro)
+  juego.actualizar(); // Llama al método `actualizar` de la clase Juego para actualizar y mostrar el estado del juego
+}
+
+function keyPressed() {
+  juego.teclapresionada(keyCode); // Llama al método `teclapresionada` cuando se presiona una tecla
+}
+
+function keyReleased() {
+  juego.teclapresionLiberada(keyCode); // Llama al método `teclapresionLiberada` cuando se suelta una tecla
+}
